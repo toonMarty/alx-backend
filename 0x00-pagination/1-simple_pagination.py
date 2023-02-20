860638a6d13c26b93b6fce7ce3293c144b7ff188
toonMarty <martinnyaga7373@gmail.com>
2023-02-20 15:00:14 +0000

    import fix

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -3,24 +3,7 @@ import csv
 import math
 from typing import List, Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple:
-    """
-    This function calculates the index_range
-    given page and page_size as inputs
-    and returns a tuple as output
-    Args:
-        page (int): the page number
-        page_size (int): the items that will be displayed in a page
-    Return:
-        Tuple: a tuple of size two containing a start index and
-            an end index corresponding to the range of indexes to
-            return in a list for those particular pagination
-            parameters.
-    """
-    start_index = (page - 1) * page_size  # offset
-    end_index = start_index + page_size
-    return start_index, end_index
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
