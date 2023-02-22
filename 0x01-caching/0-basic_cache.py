c535bcacecb08017ce171da75593b496a046b72e
toonMarty <martinnyaga7373@gmail.com>
2023-02-22 06:31:51 +0000

    Basic Disctionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+This module contains a class BasicCache that
+inherits from BaseCaching and is a caching system
+"""
+from abc import ABC
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching, ABC):
+    """
+    The class definition of BasicCache
+    """
+    def put(self, key, item):
+        """
+        This method adds a key-value element
+        to the cache_data dictionary
+        Args
+            :param key: the key of the item to add
+            :param item: the value of the item
+        Return:
+            :return: none
+        """
+        if key is not None or item is not None:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """
+        This method returns the value in self.cache_data
+        linked to key
+        Args:
+            :param key: the key to get
+        Return
+            :return: the value in self.cache_data linked to key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None
---
+++ 0x01-caching/README.md
@@ -0,0 +1,5 @@
+# 0. Basic dictionary
+A class BasicCache that inherits from BaseCaching and is a caching system
+This class has two methods:
+* put: adds an item to a dictionary
+* get: gets an item from a dictionary using the item's key
