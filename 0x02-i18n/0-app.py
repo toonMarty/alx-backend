90e38ab5d9dd80b5072a5fd4b949d43edb10627b
toonMarty <martinnyaga7373@gmail.com>
2023-02-28 18:46:42 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask app with a
+single / route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    This function returns Hello World as a header
+    """
+    return '<h1>Hello World</h1>'
